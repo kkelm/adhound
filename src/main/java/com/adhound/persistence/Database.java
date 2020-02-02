dfa71ebdbce22792dc65820e8b022adaf18ac289
Kevin Kelm <kkelm@outlook.com>
2020-02-02 14:56:45 -0600

    Created a Database class, created an AdHound database, and worked on a method to run a SQL file that creates the tables for the database. (2 Hours)

---
+++ src/main/java/com/adhound/persistence/Database.java
@@ -0,0 +1,115 @@
+package com.adhound.persistence;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.apache.ibatis.jdbc.ScriptRunner;
+import java.util.Properties;
+
+public class Database {
+
+    private Properties properties;
+
+    private Connection connection;
+    /**
+     * Private constructor prevents instantiating this class anywhere else, and calls the loadProperties method.
+     */
+    public Database() {
+        loadProperties();
+    }
+    /**
+     * Runs a SQL file to create the AdHound database.
+     */
+    private void createDatabase() {
+
+        ScriptRunner scriptRunner = new ScriptRunner(getConnection());
+
+        try {
+
+            Reader reader = new BufferedReader(new FileReader(("/adhound.sql")));
+
+            scriptRunner.runScript(reader);
+
+        } catch (FileNotFoundException e) {
+
+            System.out.println("Database.createDatabase() : Cannot load the SQL file.");
+
+        }
+
+    }
+    /**
+     * Instantiates an object of the Properties class, and loads the database properties file.
+     */
+    private void loadProperties() {
+
+        properties = new Properties();
+
+        try {
+
+            properties.load(this.getClass().getResourceAsStream("/database.properties"));
+
+        } catch (IOException e) {
+
+            System.out.println("Database.loadProperties() : Cannot load the properties file.");
+
+        }
+
+    }
+    /**
+     * Gets the Connection object to the database
+     * @return Connection object
+     */
+    public Connection getConnection() {
+        return connection;
+    }
+
+    /**
+     * Makes a connection to the database using properties.
+     * @throws SQLException
+     * @throws ClassCastException
+     */
+    public void setConnection() throws SQLException, ClassCastException {
+
+        if (connection != null) { return; }
+
+        try {
+
+            Class.forName(properties.getProperty("driver"));
+
+        } catch (ClassNotFoundException e) {
+
+            throw new ClassCastException("Database.setConnection() : MySQL Driver not found");
+
+        }
+
+        String url = properties.getProperty("url");
+
+        connection = DriverManager.getConnection(url, properties.getProperty("username"), properties.getProperty("password"));
+
+    }
+
+    /**
+     * Disconnects the connection to the database.
+     */
+    public void disconnect() {
+
+        if (connection != null) {
+
+            try {
+
+                connection.close();
+
+            } catch (SQLException e) {
+
+                System.out.println("Cannot close connection" + e);
+
+            }
+
+        }
+
+        connection = null;
+
+    }
+
+}
